942dcee1b113a60fe97fa76bba90134316d8ea74
Yannick Marcon <yannick.marcon@obiba.org>
2012-11-27 09:32:13 -0500

    R script updated with categorial variable summary

--- fs/datashield/hop.R
+++ fs/datashield/hop.R
@@ -1,7 +1,9 @@
 library('opal')
 o<-opal.login('administrator', 'password', 'http://localhost:8080')
 datashield.assign(o,'PM_BMI_CONTINUOUS','opal-data.HOP:PM_BMI_CONTINUOUS')
+datashield.assign(o,'PM_BMI_CATEGORIAL','opal-data.HOP:PM_BMI_CATEGORIAL')
 datashield.symbols(o)
 datashield.aggregate(o, 'summary(PM_BMI_CONTINUOUS)')
-#datashield.summary(o,'PM_BMI_CONTINUOUS')
+datashield.aggregate(o, 'summary(PM_BMI_CATEGORIAL)')
+
 
