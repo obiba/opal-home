cc0d2ebe884b07cf78f1ccef384e3c4b6cd3ff75
Yannick Marcon <yannick.marcon@obiba.org>
2013-02-22 17:05:18 -0500

    More datashield tests.

--- fs/home/administrator/datashield/tests/4-datashield-hop.R
+++ fs/home/administrator/datashield/tests/4-datashield-hop.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 
 #
-# Install dsbaseclient package on R client
+# Do some datashield using dsbaseclient package on R client
 #
 
 library(dsbaseclient)
@@ -11,24 +11,24 @@ o1<-opal.login('administrator', 'password', 'http://localhost:8080')
 o2<-opal.login('administrator', 'password', 'https://localhost:8443',opts=list(ssl.verifyhost=0,ssl.verifypeer=0,sslversion=3))
 opals<-list(o1,o2)
 
-print("assign some opal variable values to R symbols...")
+cat("**** assign some opal variable values to R symbols...\n")
 datashield.assign(opals,'PM_BMI_CONTINUOUS','opal-data.HOP:PM_BMI_CONTINUOUS')
 datashield.assign(opals,'PM_BMI_CATEGORIAL','opal-data.HOP:PM_BMI_CATEGORIAL')
 datashield.assign(opals,'PM_HEIGHT_MEASURE','opal-data.HOP:PM_HEIGHT_MEASURE')
 datashield.assign(opals,'PM_WEIGHT_MEASURE','opal-data.HOP:PM_WEIGHT_MEASURE')
 datashield.assign(opals,'SEX','opal-data.HOP:GENDER')
 
-print("get symbol length:")
+cat("**** get symbol length:\n")
 datashield.length(opals, as.symbol('PM_BMI_CATEGORIAL'))
 
-print("display the symbols:")
+cat("**** display the symbols:\n")
 datashield.symbols(opals)
 
-print("get some summaries:")
+cat("**** get some summaries:\n")
 datashield.aggregate(opals, 'summary(PM_BMI_CONTINUOUS)')
 datashield.aggregate(opals, 'summary(PM_BMI_CATEGORIAL)')
 
-print("other ways to get the summary:")
+cat("**** other ways to get the summary:\n")
 datashield.aggregate(opals, call('summary', as.symbol('PM_BMI_CATEGORIAL')))
 datashield.summary(opals, as.symbol('PM_BMI_CATEGORIAL'))
 
